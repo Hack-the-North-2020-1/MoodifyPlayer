4b50b83d5881c6794719a7de378434c6c739c1d6
Ryan Bui <buihuyryan@gmail.com>
2021-01-16 16:45:01 -0500

    feat(): Integrate mood with bing image query search

--- controllers/home.py
+++ controllers/home.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, session, request
 
+from services.query_image import BingImageQuery
 from services.spotify import Spotify
 from services.predict_mood import MusicToMood
 from settings import SPOTIFY_CLIENT_SECRET, SPOTIFY_CLIENT_ID
@@ -17,11 +18,12 @@ def home():
             track = request.form['song_request']
             artist = request.form['artist']
             query = f'track:{track} artist:{artist}'
-            print(spotify.search(query)['tracks']['items'][0])
             id = spotify.search(query)['tracks']['items'][0]['id']
             music_to_mood = MusicToMood(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
 
             data = music_to_mood.predict_mood(id)
-            return data
+
+            image_query = BingImageQuery()
+            image_query.query_images(f'{data}')
 
     return render_template("home/home.html")
