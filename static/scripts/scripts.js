557729dbf1ca0ffe224d7c5d98824731349df88a
VioletZhouECE <x375zhou@uwaterloo.ca>
2021-01-17 05:00:33 -0500

    fetch data from backend

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,5 +1,7 @@
 var imageContainer = document.getElementById("image-container");
 var image = document.getElementById("image-placeholder");
+var songnameContainer = document.getElementById("songname");
+var artistnameContainer = document.getElementById("artistname");
 
 function subscribeToImages(updateImage){
     //connect to the hashtag namespace
@@ -11,6 +13,28 @@ function subscribeToImages(updateImage){
 function updateImage(data){
     console.log(data);
     image.src = data;
+}
+
+function playsong(event){
+    songname = songnameContainer.value;
+    artistname = artistnameContainer.value;
+
+    token = "BQAw4Mnaa1B0s_7jgPmQ9fu85rvkmD4qHnW2qZ8Gr9E2gef9iVNI1HF4ICRjVOdz8PnYZn1dQDc5FgbLvEWTX1EK8mLLNOdaoMfOl2Gsmwh6sw7nYXtpHg3XAbOwPmqMMXcqMamX1Utt7QOVA8VRvgHtUqyGcbe3soJsM_AK894"
+    
+    fetch(`/artist:${artistname}&song:${songname}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type' : 'application/json',
+            'Authorization' : 'Bearer ' + token
+        }
+    })
+    .then(data=> {
+        console.log(data);
+     }
+    )
+    .catch(err=>{
+        console.log(err);
+    })
 }
 
 subscribeToImages(updateImage);
